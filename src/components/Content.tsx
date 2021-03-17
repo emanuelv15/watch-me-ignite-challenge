771f591e958cbaee777c0ff57d02399cf5851a72
Emanuel Vitor <emanuel.v.souza@hotmail.com>
2021-03-16 23:43:15 -0300

    componente SideBar separado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,44 @@
+import { useEffect, useState } from 'react';
+
+import { MovieCard } from './MovieCard';
+import { api } from '../services/api';
+
+import '../styles/content.scss';
+
+interface MovieProps {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
 export function Content() {
   // Complete aqui
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${1}`).then(response => {
+      setMovies(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {1}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+    </div>
+  )
 }
